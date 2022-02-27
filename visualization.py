255d001dd409536be649515855235f39d2320d7f
Jazna Meza Hidalgo <99938467+jaznamezahidalgo@users.noreply.github.com>
2022-02-26 21:54:38 -0300

    Se agrega un gráfico de torta

--- visualization.py
+++ visualization.py
@@ -11,7 +11,7 @@ class Visualization(object):
   def __setattr__(self, name: str, value):
     self.__dict__[name] = value 
 
-  def histogram(self, column, num_bins, title, labels):
+  def histogram(self, column :str, num_bins : int, title: str, labels : np.array):
     # Tamaño del grafico: (ancho, largo)
     plt.figure(figsize=(8,8))
     # Definimos a Matplotlib que es un histograma con los valores de y agrupados en num_bis
@@ -24,7 +24,7 @@ class Visualization(object):
 
     plt.show()        
 
-  def piechart_by_interval(self, title :str,column : str, exp : np.array):
+  def piechart_by_interval(self, title :str, column : str, exp : np.array):
     # Obtiene los valores
     values = self.data_frame[column]
     # Obtiene los límites
@@ -42,11 +42,17 @@ class Visualization(object):
     frecuencias[3] = values.loc[values >= q3].count()   
 
     # Etiqueta de cada trozo
-    m = ['Menos de {} millones'.format(q1),'Entre {0} y {1} millones'.format(q1, q2),
-        'Entre {0} y {1} millones'.format(q2, q3), '{} millones o más'.format(q3)]
+    m = ['Menos de {}'.format(q1),'Entre {0} y {1}'.format(q1, q2),
+        'Entre {0} y {1}'.format(q2, q3), '{} o más'.format(q3)]
 
     plt.figure(figsize=(10,10))
 
     plt.title(title, fontsize=18, fontweight="bold")
     plt.pie(frecuencias, labels = m, explode = exp, autopct='%2.1f%%')
+    plt.show()     
+
+  def piechart(self, column : str, title : str, labels : np.array):
+    plt.figure(figsize=(8,8))
+    plt.pie(self.data_frame[column].value_counts(), labels = labels, autopct='%2.1f%%')
+    plt.title(title, fontsize=18, fontweight="bold")
     plt.show()     
